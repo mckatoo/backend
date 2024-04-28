346ecf06f56898b2ddc7fe2f91958cfc1f4fdc53
Milton Carlos Katoo <mckatoo@gmail.com>
2024-04-28 14:26:40 -0300

    added skills relation on project serializer

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from projects.models import Projects
+from skills.serializers import SkillSerializer
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    skills = SkillSerializer(many=True, read_only=True)
+
     class Meta:
         model = Projects
         fields = "__all__"
