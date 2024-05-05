520351e069e14d773a8bb4a64445df6d11abec20
Milton Carlos Katoo <mckatoo@gmail.com>
2024-05-05 10:48:09 -0300

    removed method save and set auto add on date type fields

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.timezone import now
 
 from skills.models import Skills
 
@@ -13,14 +12,9 @@ class Projects(models.Model):
     description = models.TextField(blank=False, null=False)
     snapshot = models.CharField(max_length=200, blank=False, null=False)
     repository_link = models.URLField(blank=False, null=False)
-    start = models.DateField()
-    last_update = models.DateField()
+    start = models.DateField(auto_now_add=True)
+    last_update = models.DateField(auto_now=True)
     skills = models.ManyToManyField(Skills)
-
-    def save(self, *args, **kargs):
-        if not self.start:
-            self.start = now()
-        return super(Projects, self).save(*args, **kargs)
 
     def __str__(self):
         return f"{self.pk} - {self.title}"
